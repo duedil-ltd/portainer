4f5a8dc07688d0a220c1beb74a82364cf410a8d8
Tom Arnfeld <tarnfeld@me.com>
2014-09-02 09:21:25 +0100

    Support for .dockerignore

--- portainer/app/scheduler.py
+++ portainer/app/scheduler.py
@@ -11,6 +11,7 @@ import tempfile
 import threading
 import uuid
 
+from fnmatch import fnmatch
 from functools import partial
 from fs.opener import opener
 from pesos.vendor.mesos import mesos_pb2
@@ -114,6 +115,8 @@ class Scheduler(mesos.interface.Scheduler):
                             offer_cpu = float(resource.scalar.value)
                         if resource.name == "mem":
                             offer_mem = int(resource.scalar.value)
+
+                    logger.debug("Received offer for cpus:%f mem:%d", offer_cpu, offer_mem)
 
                     # Look for a task in the queue that fits the bill
                     to_launch = []
@@ -336,6 +339,7 @@ class Scheduler(mesos.interface.Scheduler):
         self.task_ids[task_id] = build_task
 
         logger.info("Prepared task %s to build %s", task_id, path)
+        logger.debug("%s", build_task)
 
         return task
 
@@ -351,6 +355,7 @@ class Scheduler(mesos.interface.Scheduler):
                 local_path, remote_path = instruction
                 tar_path = "context/%s" % str(idx)
 
+                # TODO(tarnfeld): This isn't strict enough
                 if local_path.startswith("http"):
                     continue
 
@@ -358,10 +363,31 @@ class Scheduler(mesos.interface.Scheduler):
                     local_path = os.path.join(context_root, local_path)
                 local_path = os.path.abspath(local_path)
 
-                logger.debug("Adding path %s to tar at %s", local_path, tar_path)
+                # Maintain a set of globs to ignore
+                ignore = {os.path.join(local_path, ".dockerignore")}
 
-                # TODO(tarnfeld): Take note of .dockerignore files
-                tar.add(local_path, arcname=tar_path)
+                for (dirpath, _, filenames) in os.walk(local_path, followlinks=True):
+
+                    # Update the set of ignored paths with any new .dockerignore files we see
+                    ignore_path = os.path.join(dirpath, ".dockerignore")
+                    if os.path.exists(ignore_path):
+                        with open(ignore_path, 'r') as f:
+                            for line in f:
+                                line = line.strip()
+                                if line and not line.startswith("#"):
+                                    ignore.add(os.path.join(dirpath, line) + "*")
+
+                    for filename in filenames:
+                        path = os.path.join(dirpath, filename)
+                        for expr in ignore:
+                            if fnmatch(path, expr):
+                                logger.debug("Ignoring path %s", path)
+                                break
+                        else:
+                            rel_path = path.replace(local_path, '').lstrip('/')
+                            logger.debug("Adding path %s to tar in %s", rel_path, tar_path)
+                            tar.add(path, arcname=os.path.join(tar_path, rel_path))
+
                 dockerfile.instructions[idx] = (cmd, (tar_path, remote_path))
 
         # Write the modified dockerfile into the tar also
@@ -372,7 +398,7 @@ class Scheduler(mesos.interface.Scheduler):
             if cmd not in dockerfile.INTERNAL:
                 line = "%s %s" % (cmd, " ".join(instructions))
 
-                logger.debug("Added command %r to new dockerfile", line)
+                logger.debug("Adding instruction %r to dockerfile", line)
                 buildfile.write("%s\n" % line)
 
         buildfile.seek(0, os.SEEK_END)

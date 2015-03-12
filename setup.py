f0deb352f3a1859649122cdd9186b79fcd93bbbe
Owen Smith <owen@duedil.com>
2015-03-12 15:56:28 +0000

    Build the executor as a standalone pex

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 """Minimal setup.py, not yet ready for publishing but just for pex packaging"""
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='portainer',
     package_dir={
         'portainer': 'portainer'
     },
-    packages=[
-        'portainer'
-    ]
+    packages=find_packages()
 )

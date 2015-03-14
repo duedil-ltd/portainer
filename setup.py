e01d0c3a5e206cfb201b0302c39a67832bc9da4e
Owen Smith <owen@duedil.com>
2015-03-14 15:09:02 +0000

    Make pex building work in the vagrant:
    ...and generally make it faster and friendlier:
    
    - make pex building use its own separate virtualenv with the correct version incantations
    - make setup.py parametric to allow that
    - hack for virtualbox not allowing hardlinks
    - simplify tar building
    
    Conflicts:
    	requirements.pip

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 """Minimal setup.py, not yet ready for publishing but just for pex packaging"""
 from setuptools import setup, find_packages
 
+# Fix to build sdist under vagrant, where hardlinks fail
+# http://bugs.python.org/issue8876
+import os
+if 'vagrant' in str(os.environ):
+    del os.link
+
 setup(
     name='portainer',
     package_dir={

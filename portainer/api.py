f54c5bbdc4aad967f82f1bb4869254d09d58a67f
Tom Arnfeld <tarnfeld@me.com>
2015-08-31 16:57:10 +0100

    Implemented async callbacks for uploads and log streams

--- portainer/api.py
+++ portainer/api.py
@@ -1,7 +1,10 @@
 # import logging
 import json
-import tempfile
 import tarfile
+import tempfile
+import threading
+import uuid
+from Queue import Queue
 
 import tornado.ioloop
 import tornado.web
@@ -32,6 +35,8 @@ class BuildHandler(tornado.web.RequestHandler):
 
     def prepare(self):
         self.tmp = tempfile.TemporaryFile()
+        self.building = threading.Event()
+        self.msg_stream = Queue()
 
     def data_received(self, data):
         self.tmp.write(data)
@@ -42,55 +47,144 @@ class BuildHandler(tornado.web.RequestHandler):
         try:
             dockerfile_path = self.get_argument("dockerfile", "Dockerfile")
             repository_tag = self.get_argument("t", None)
-            verbose = self.get_argument("q", True)
+            verbose = not self.get_argument("q", True)  # TODO: Handle casting to bool?
             build_mem = self.get_argument("memory", 0)
             build_cpu = float(self.get_argument("cpushares", 1))
 
             if not repository_tag:
                 self.set_status(500)
-                self._send(error="Missing repository name for the docker image, use -t")
+                self._send(err="Missing repository name for the docker image, use -t")
                 return
             if not build_mem:
                 self.set_status(500)
-                self._send(error="No build memory limit set (megabytes)")
+                self._send(err="No build memory limit set (megabytes)")
                 return
 
             try:
+                if verbose:
+                    self._send(msg="DEBUG: Opening context")
                 self.tmp.seek(0)
                 tar = tarfile.TarFile(fileobj=self.tmp)
-                raw_dockerfile = tar.extractfile(dockerfile_path)
+                if verbose:
+                    self._send(msg="Reading Dockerfile from %r" % dockerfile_path)
+                tar.extractfile(dockerfile_path)
             except:
                 self.set_status(500)
-                self._send(error="Failed to load Dockerfile from tar context %r" % dockerfile_path)
+                self._send(err="Failed to load Dockerfile from tar context %r" % dockerfile_path)
                 return
 
             self.set_status(200)
-            self._send(message="Processing submitted build context")
+            self._send(msg="Processing submitted build context")
 
-            # Parse the Dockerfile to ensure it's valid
-            # try:
-            #     parse_dockerfile_fp(raw_dockerfile)
-            # except:
-            #     self._send(error="Failed to parse Dockerfile")
+            task_id = str(uuid.uuid1())
+            staging_dir = os.path.join("staging", task_id)
+            self.fs.makedir(staging_dir, recursive=True)
 
-            # TODO: Create a staging directory for the build tar
-            # TODO: Upload the build tar
-            # TODO: Queue the build and watch for logs to be streamed back from the scheduler
-            # TODO: Handle killing the build if the connection is lost to the client
+            self.tmp.seek(0)
+            self.building.set()
+
+            staging_context = os.path.join(staging_dir, "context.tar.gz")
+            self.filesystem.setcontents_async(
+                path=staging_context,
+                data=self.tmp,
+                progress_callback=partial(
+                    self._ctx_upload_progress,
+                    task_id,
+                    verbose
+                ),
+                finished_callback=partial(
+                    self._ctx_upload_finished,
+                    task_id,
+                    repository_tag,
+                    build_mem,
+                    build_cpu,
+                    verbose
+                ),
+                error_callback=partial(
+                    self._ctx_upload_failed,
+                    task_id,
+                    verbose
+                )
+            )
+
+            self._stream_messages()
+
         finally:
+            self.building.clear()
+            self.msg_stream.close()
             self.tmp.close()
 
-    def _send(self, message=None, error=None):
-        if message:
+    def _send(self, msg=None, err=None):
+        if msg and err:
+            raise ArgumentError
+        if msg:
             self.write(json.dumps({
-                "stream": "%s\n" % message
+                "stream": "%s\n" % msg
             }))
             self.flush()
-        elif error:
+        elif err:
             self.write(json.dumps({
-                "error": "%s\n" % error
+                "error": "%s\n" % err
             }))
             self.flush()
+
+    def _stream_messages(self):
+        """
+        Wait for the build to finish and stream messages back to the client as
+        they appear on the stream.
+        """
+
+        while self.building.is_set():
+            msg = self.msg_stream.get(True, 0.1)
+            if not msg:
+                continue
+
+            message, is_error = msg
+            if is_error:
+                self._send(err=message)
+            else:
+                self._send(msg=msg)
+
+    def _ctx_upload_progress(self, task_id, verbose, total, bytes):
+        """
+        Callback for progress updates to the build context upload.
+        """
+
+        # TODO: Show some sort of progress information? Perhaps look into how
+        # Docker does the streaming progress bars.
+        pass
+
+    def _ctx_upload_finished(self, task_id, repository_tag, build_mem, build_cpu, verbose):
+        """
+        Callback for when the build context upload finishes. When this callback
+        is fired, the actual build is placed on the queue with relevant parameters.
+        """
+
+        self.msg_stream.put(("Finished uploaded build context", False))
+
+        build_task = portainer_pb2.BuildTask()
+        build_task.stream = vebose
+        build_task.context = None  # TODO
+
+        repo = parse_repository(repository_tag)
+        build_task.image.registry.hostname = repo.registry_host
+        if repo.registry_port:
+            build_task.image.registry.port = repo.registry_port
+
+        build_task.image.repository = repo.name
+        build_task.image.tag.append(repo.tag)
+
+        self.queue.put((build_task, self.building, self.msg_stream))
+
+    def _ctx_upload_failed(self, task_id, verbose, exc):
+        """
+        Callback for when the build context upload fails.
+        """
+
+        self.msg_stream.put(("Caught exception while uploading the build context", True))
+        self.building.clear()
+
+        raise exc
 
     # def on_connection_close(self):
     #     # TODO: Kill any builds in progress
